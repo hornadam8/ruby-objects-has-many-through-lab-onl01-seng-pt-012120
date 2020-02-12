4cf3b7ae56a8d6396774492cd541c8c0b1e5924f
Adam G Horn <hornadam8@gmail.com>
2020-02-12 13:32:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
-c
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name,artist,genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+  
+end
